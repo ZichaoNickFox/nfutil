688b07cd20e96579a3c17b35d08caf1686a771c5
liuzichao <384914453@qq.com>
2019-11-17 14:41:16 +0800

    不做成库

---
+++ build/CMakeFiles/nfutil.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nfutil.dir/src/log.cpp.o"
+  "libnfutil.pdb"
+  "libnfutil.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nfutil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/nfutil_dynamic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nfutil_dynamic.dir/src/log.cpp.o"
+  "libnfutil.pdb"
+  "libnfutil.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nfutil_dynamic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
